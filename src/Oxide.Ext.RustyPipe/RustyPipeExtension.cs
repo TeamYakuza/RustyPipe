b2e70eb960ea40a3cefa2617a6897bc9b48e986b
Ultimation <>
2019-10-07 13:01:51 +0200

    Initial Commit

---
+++ src/Oxide.Ext.RustyPipe/RustyPipeExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oxide.Core;
+using Oxide.Core.Extensions;
+
+namespace Oxide.Ext.RustyPipe
+{
+    public class RustyPipeExtension:Extension
+    {
+        public RustyPipeExtension(ExtensionManager manager) : base(manager)
+        {
+        }
+
+        public override string Name => "RustyPipe";
+        public override string Author => "Team Yakuza";
+        public override VersionNumber Version => new VersionNumber(1,0,0);
+    }
+}
